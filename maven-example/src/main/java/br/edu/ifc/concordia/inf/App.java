88fac4ca8366b5081f5beb2059e3971a8cadfe5e
Renato Oliveira <renatorro@comp.ufla.br>
2017-05-03 09:36:32 -0300

    Introdução ao maven e hibernate

---
+++ maven-example/src/main/java/br/edu/ifc/concordia/inf/App.java
@@ -0,0 +1,41 @@
+package br.edu.ifc.concordia.inf;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import br.edu.ifc.concordia.inf.models.User;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Configuration config = new Configuration();
+		config.configure("hibernate.cfg.xml");
+		StandardServiceRegistry serviceRegistry =
+				new StandardServiceRegistryBuilder()
+				.applySettings(config.getProperties()).build();
+		Metadata metadata = new MetadataSources( serviceRegistry )
+				.addAnnotatedClass(User.class)
+				.buildMetadata();
+		SessionFactory factory = metadata.buildSessionFactory();
+		Session session = factory.openSession();
+		
+		User user = new User();
+		user.setName("Renato");
+		user.setEmail("renato@exemplo.com");
+		user.setPassword("12345");
+		session.persist(user);
+		
+		System.out.printf("ID Atribuido: %d\n", user.getId());
+		session.close();
+		factory.close();
+	}
+}
