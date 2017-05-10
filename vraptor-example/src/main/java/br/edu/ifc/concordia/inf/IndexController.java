0a74b066a8dff07945fb5ce78936050e6143b549
Renato Oliveira <renatorro@comp.ufla.br>
2017-05-10 11:31:04 -0300

    Setup da aplicação e utilização dos escopos.

--- vraptor-example/src/main/java/br/edu/ifc/concordia/inf/IndexController.java
+++ vraptor-example/src/main/java/br/edu/ifc/concordia/inf/IndexController.java
@@ -1,30 +1,34 @@
-package br.com.caelum.vraptor.controller;
+package br.edu.ifc.concordia.inf;
 
 import javax.inject.Inject;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.boilerplate.HibernateDAO;
 
 @Controller
 public class IndexController {
 
 	private final Result result;
+	private final HibernateDAO dao;
 
 	/**
 	 * @deprecated CDI eyes only
 	 */
 	protected IndexController() {
-		this(null);
+		this(null, null);
 	}
 	
 	@Inject
-	public IndexController(Result result) {
+	public IndexController(Result result, HibernateDAO dao) {
 		this.result = result;
+		this.dao = dao;
 	}
 
 	@Path("/")
 	public void home() {
+		
 		result.include("variable", "VRaptor!");
 	}
 }
