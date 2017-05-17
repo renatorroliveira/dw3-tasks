67d399b3e5f1e35f1b78ed9b06031da1a98b662d
Renato Oliveira <renatorro@comp.ufla.br>
2017-05-17 16:51:02 -0300

    Reformulação do projeto

---
+++ tasks-viewer/src/main/java/br/edu/ifc/concordia/inf/IndexController.java
@@ -0,0 +1,18 @@
+package br.edu.ifc.concordia.inf;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.boilerplate.NoCache;
+import br.edu.ifc.concordia.inf.abstractions.AbstractController;
+
+@Controller
+public class IndexController extends AbstractController {
+	
+	
+	@Path(value="/", priority=Path.HIGHEST)
+	@NoCache
+	public void index() {
+		
+	}
+	
+}
